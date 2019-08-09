e175323a5d5f30d7807beac6e3062f4f616c36aa
unknown <sybren.deuzeman@gmail.com>
2019-08-09 10:24:20 +0200

    Remove save_iots function as it did not work properly

--- R/save_iots.R
+++
@@ -1,9 +0,0 @@
-
-#' @import tcltk
-#' @export
-save_iots <- function(iots, filename = "choose"){
-  if (filename == "choose"){
-    filename <- tclvalue(tkgetSaveFile(filetypes = "{ {Comma Seperated Values} {*.rds} }", defaultextension = ".rds", initialdir = getwd()))
-  }
-  saveRDS(iots, filename)
-}
